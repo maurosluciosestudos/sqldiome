6413fa3adc37986d8b7976ecf21d18501dfd6416
maurosluciosestudos <maurosluciosestudos@gmail.com>
2024-03-24 14:33:37 -0300

    inserindo valores na tabela destino

--- sql/destino.sql
+++ sql/destino.sql
@@ -1,4 +1,7 @@
 create table destino(id int(11) auto_increment primary key,
     -> nome varchar(50) not null,
     -> descricao text)engine=InnoDB default charset="utf8";
-Query OK, 0 rows affected, 2 warnings (2.31 sec)
+    
+insert into destino VALUES(null,'destino 1','destino 1 muito bonito e com alto custo beneficio');
+
+insert into destino VALUES(null,'destino 2','destino 2 muito bonito e com alto cutso beneficio: parais, bares, shopping tudo acessivel');

6b9d4a1da4df74a081991ee09e7202cae4021ce3
maurosluciosestudos <maurosluciosestudos@gmail.com>
2024-03-24 15:41:57 -0300

    inserindo dados em reserva

--- sql/reserva.sql
+++ sql/reserva.sql
@@ -1,2 +1,5 @@
 create table reserva(id int(11) auto_increment primary key, id_usuario int(11) not null, id_destino int(11) not null, data_reserva date, status varchar(1) default(0), Foreign Key(id_usuario) references usuario(id), Foreign Key(id_destino) references
  destino(id))engine=InnoDB default charset="utf8";
+
+ insert into reserva VALUES(null,1,1,"2024-12-31");
+ insert into reserva VALUES(null,2,2,"2024-12-31");

0b3607fb57c7ee2e3c127f2f69b9d8fd8c6be6f5
maurosluciosestudos <maurosluciosestudos@gmail.com>
2024-03-24 14:33:51 -0300

    inserindo valores na tabela endereco

--- sql/endereco.sql
+++ sql/endereco.sql
@@ -6,3 +6,7 @@ create table endereco(id int(11) auto_increment primary key,
     -> estado varchar(30) not null,
     -> uf char(5) not null,
     -> Foreign Key(usuario_id) references usuario(id))engine=InnoDB default charset="utf8";
+    
+    insert into endereco VALUES(null,1,"rua usuario 1","10","cidade usuario 1","estado usuario 1","RJ");
+
+    insert into endereco VALUES(null,2,"rua usuario 2","20","cidade usuario 2","estado usuario 2","RJ");
